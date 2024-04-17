edc739fb5218ea92d888d829853b6a02c8811cb3
LazarevSlava <549132w@gmail.com>
2024-04-17 14:47:28 +0200

    created header

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import style from './Header.module.css';
+import { useState } from 'react';
+import logo from '../../images/logo.svg?react';
+
+function Header() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuVisible((prev) => !prev);
+  };
+  return (
+    <div className={style.head_block}>
+      <div className={style.head}>
+        <img src={logo} alt="logo" />
+        <button className={style.btn_menu} onClick={toggleMenu}></button>
+      </div>
+      <div className={`${style.menu} ${isMenuVisible ? '' : style.hidden}`}>
+        <button className={style.btn}>Features</button>
+        <button className={style.btn}>Pricing</button>
+        <button className={style.btn}>Resources</button>
+        <hr className={style.hr} />
+        <button className={style.btn}>Login</button>
+        <button className={style.btn}>sign Up</button>
+      </div>
+    </div>
+  );
+}
+
+export { Header };
