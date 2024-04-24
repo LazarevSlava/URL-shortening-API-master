541540ae516b94d499e0653598fcfb3f16adab01
LazarevSlava <549132w@gmail.com>
2024-04-24 09:53:49 +0200

    lint

--- src/components/elements/Title.jsx
+++ src/components/elements/Title.jsx
@@ -4,8 +4,8 @@
 //   - 'big'
 import style from './Title.module.css';
 
-function Title({ children, tag='h1', size = 'normal' }) {
-  const Tag=tag;
+function Title({ children, tag = 'h1', size = 'normal' }) {
+  const Tag = tag;
   return <Tag className={style[size]}>{children}</Tag>;
 }
 export { Title };
